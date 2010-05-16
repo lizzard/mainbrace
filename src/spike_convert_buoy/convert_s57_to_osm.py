fbb261a7609af49e8bd71896f21d3d14234546e3
Danny O'Brien <dobrien@cpj.org>
2010-05-15 21:31:39 -0400

    changed to new list naming scheme

--- src/spike_convert_buoy/convert_s57_to_osm.py
+++ src/spike_convert_buoy/convert_s57_to_osm.py
@@ -40,16 +40,18 @@ def s57_to_osm(feature, layer_name=None):
     # grab CATSPM=
 
     # CATSPM = 27     seamark:buoy_special_purpose:category=general_warning_mark
-    osm_tags['seamark:buoy_special_purpose:category'] = catspm(feature.CATSPM)
+    catspm_values = []
+    osm_tags['seamark:buoy_special_purpose:category'] = catspm_values(feature.CATSPM)
     # OBJNAM=SF Airport  Buoy N         name=(name)       seamark:name=(name)
     osm_tags['name'] = feature.OBJNAM
     osm_tags['seamark:name'] = feature.OBJNAM
     # STATUS=8      buoy:status=private
-    osm_tags['buoy:status'] = status(feature.STATUS)
+    status_values = []
+    osm_tags['buoy:status'] = status_values(feature.STATUS)
     # SCAMIN=120000   buoy:scale_minimum
     osm_tags['buoy:scale_minimum']=feature.SCAMIN
     # SORDAT=20040914       source:date=20040914
-    # How are dates stored in open seamap
+    # How are dates stored in open seamap??
     # POINT (-122.381666999999993 37.640278000000002)    latitude=  37.640278000000002      longitude=-122.381666999999993
     
     return osm_tags
